81cb884483384200a9bfb485cde4f8abb40a30e8
pde-bakk <pde-bakk@student.codam.nl>
2022-01-30 19:07:20 +0100

    fixed reading in filenames

--- predict.py
+++ predict.py
@@ -2,11 +2,11 @@ import sys
 from linear_regression import LinearRegression
 
 
-def predict_car_price(filename: str):
+def predict_car_price(theta_filename: str, data_filename: str):
 	linreg = LinearRegression()
 	try:
-		linreg.load_data('data/data.csv')
-		linreg.load_thetas(filename)
+		linreg.load_data(data_filename)
+		linreg.load_thetas(theta_filename)
 	except FileNotFoundError:
 		print(f'Please supply a valid filename', file = sys.stderr)
 		return
@@ -17,7 +17,7 @@ def predict_car_price(filename: str):
 		try:
 			mileage = int(input('Please type the mileage in kilometers: '))
 			price = linreg.predict(mileage)
-			print(f'I predict that a car with {mileage} km mileage will have a price of ${price}')
+			print(f'I predict that a car with {mileage} km mileage will have a price of €{int(price)}')
 		except ValueError:
 			print('Please input a valid mileage in kilometers (please give an int).', file = sys.stderr)
 		except KeyboardInterrupt:
@@ -25,7 +25,7 @@ def predict_car_price(filename: str):
 
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
+	if len(sys.argv) < 3:
 		print(f'Usage: python3 predict.py [thetas.csv] [data.csv]', file = sys.stderr)
 	else:
-		predict_car_price(sys.argv[1])
+		predict_car_price(*sys.argv[1:])
