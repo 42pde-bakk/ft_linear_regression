7232c48697c377ff7a0e566568e2d6ae4161dc0c
pde-bakk <pde-bakk@student.codam.nl>
2022-02-06 16:08:40 +0100

    moved parse_arguments to own file

--- mean_squared_error.py
+++ mean_squared_error.py
@@ -3,12 +3,17 @@ import sys
 from math import pow
 
 from linear_regression import LinearRegression
+from parse_args import parse_arguments
 
 
 def calculate_error(args: argparse.Namespace):
 	linreg = LinearRegression()
-	linreg.load_data(args.data_file)
-	linreg.load_thetas(args.thetas_file)
+	try:
+		linreg.load_data(args.data_file)
+		linreg.load_thetas(args.thetas_file)
+	except AssertionError:
+		print(f'Supplied thetas file ({args.thetas_file}) is invalid.', file = sys.stderr)
+		return
 
 	sum_total = sum_of_errors_squared = 0
 	for row in linreg.data:
@@ -22,22 +27,6 @@ def calculate_error(args: argparse.Namespace):
 		print(f'Total mean squared: {sum_total}, mean squared error: {mse}')
 	pct = 100 - mse / sum_total * 100
 	print(f'Gives an accuracy of {pct:.2f}% (when within the limits of the training dataset, mind you).')
-
-
-def parse_arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('data_file', nargs = '?', action = 'store', help = 'Data.csv', default = 'data.csv')
-	parser.add_argument('thetas_file', nargs = '?', action = 'store', help = 'File for the theta values', default = 'thetas.csv')
-	parser.add_argument('--verbose', '-v', action = 'store_true', help = 'Show the mean squared error')
-
-	a = parser.parse_args()
-	if not a.thetas_file or not a.data_file:
-		if not a.thetas_file:
-			print(f'Please supply a valid thetas file', file = sys.stderr)
-		else:
-			print(f'Please supply a valid data.csv file', file = sys.stderr)
-		exit(1)
-	return a
 
 
 if __name__ == '__main__':
--- predict.py
+++ predict.py
@@ -1,11 +1,13 @@
 import argparse
 import sys
 import time
+import numpy as np
 
 from linear_regression import LinearRegression
+from parse_args import parse_arguments
 
 
-def prompt_bonus(linreg: LinearRegression) -> tuple[int, int]:
+def prompt_bonus(linreg: LinearRegression) -> tuple[np.ndarray, int]:
 	params = list(map(int, input(f'Please type the input parameters ({", ".join(linreg.columns[:-1])}): ').split()))
 	if len(params) != len(linreg.columns) - 1:
 		print(f'Error. Expecting {len(linreg.columns) - 1} values, got {len(params)}.')
@@ -13,20 +15,21 @@ def prompt_bonus(linreg: LinearRegression) -> tuple[int, int]:
 	if any(param < 0 for param in params):
 		raise ValueError
 	price = linreg.predict(params)
-	return params[0], int(price)
+	params.append(price)
+	return np.array(params, dtype = float), int(price)
 
 
-def prompt(linreg: LinearRegression) -> tuple[int, int]:
+def prompt(linreg: LinearRegression) -> tuple[np.ndarray, int]:
 	mileage = int(input('Please type the mileage in kilometers: '))
 	if mileage < 0:
 		raise ValueError
 	price = linreg.predict(mileage)
-	return mileage, int(price)
+	return np.array([mileage, price], dtype = float), int(price)
 
 
 def predict_car_price(args: argparse.Namespace) -> None:
 	linreg = LinearRegression()
-	inputs, outputs = [], []
+	preds = []
 	try:
 		linreg.load_data(args.data_file)
 		linreg.load_thetas(args.thetas_file)
@@ -37,12 +40,11 @@ def predict_car_price(args: argparse.Namespace) -> None:
 	while True:
 		try:
 			if bonus:
-				a, b = prompt_bonus(linreg)
+				arr, price = prompt_bonus(linreg)
 			else:
-				a, b = prompt(linreg)
-			print(f'I predict €{b}.')
-			inputs.append(a)
-			outputs.append(b)
+				arr, price = prompt(linreg)
+			print(f'I predict €{price}.')
+			preds.append(arr)
 		except ValueError:
 			print('Please input valid parameters.', file = sys.stderr, flush = True)
 			time.sleep(0.5)  # Otherwise stderr doesn't flush, ikr
@@ -50,24 +52,8 @@ def predict_car_price(args: argparse.Namespace) -> None:
 			time.sleep(0.5)
 		except KeyboardInterrupt:
 			if args.verbose:
-				linreg.plot_predictions(inputs, outputs)
+				linreg.plot_predictions(preds = np.array(preds))
 			break
-
-
-def parse_arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('data_file', nargs = '?', action = 'store', help = 'Data.csv', default = 'data.csv')
-	parser.add_argument('thetas_file', nargs = '?', action = 'store', help = 'File for the theta values', default = 'thetas.csv')
-	parser.add_argument('--verbose', '-v', action = 'store_true', help = 'Show the mean squared error')
-
-	a = parser.parse_args()
-	if not a.thetas_file or not a.data_file:
-		if not a.thetas_file:
-			print(f'Please supply a valid thetas file', file = sys.stderr)
-		else:
-			print(f'Please supply a valid data.csv file', file = sys.stderr)
-		exit(1)
-	return a
 
 
 if __name__ == '__main__':
